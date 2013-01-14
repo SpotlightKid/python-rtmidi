06a24cbfbfd545042dfa16d7099bb76d0a2f39c0
Christopher Arndt <SpotlightKid@users.noreply.github.com>
2013-01-14 18:35:56 +0000

    Some more corrections re Cython version in release info

--- rtmidi/release.py
+++ rtmidi/release.py
@@ -140,10 +140,8 @@ platform-specific hints below.
 
 Only if you want to change the Cython source file ``_rtmidi.pyx`` or want to
 recompile ``_rtmidi.cpp`` with a newer Cython version, you'll need to install
-Cython >= 0.17. Currently this version is only available via the Git
-respository (see Cython web site) as version 0.17pre. The ``_rtmidi.cpp`` file
-in the  source distribution was compiled with Cython 0.17pre as of 2013-01-14
-and Python 2.7.2.
+Cython >= 0.17. The ``_rtmidi.cpp`` file in the  source distribution was
+generated with Cython 0.18pre as of 2013-01-14 and Python 2.7.3.
 
 RtMidi (and therefore ``python-rtmidi``) supports several low-level MIDI
 libraries on different operating systems. Only one of the available options
