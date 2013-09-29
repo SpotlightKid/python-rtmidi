916c15d5c0034cf7d0da9977667619050dfb05e7
carndt <carndt@d91bdeee-4e13-0410-98cf-97065b31a653>
2013-09-29 17:17:47 +0000

    Make `sendsysex.py` example Python 3 compatible and restructure it to ce more consistent with the other example scripts

--- projects/python-rtmidi/trunk/examples/sendsysex.py
+++ projects/python-rtmidi/trunk/examples/sendsysex.py
@@ -3,41 +3,118 @@
 #
 # sendsysex.py
 #
-"""Send all system exclusive files given on the command line or found in the 
-current directory to the chosen MIDI ouput after confirmation.
+"""Send all system exclusive files given on the command line.
+
+The paths given on the command line can also contain directories and all files
+with a *.syx extension in them will be sent (in alphabetical order).
+
+All consecutive sysex messages in each file will be sent to the chosen MIDI
+ouput, after confirmation (which can be turned off).
 
 """
 
+__program__ = 'sendsysex.py'
+__version__ = '1.1 ($Rev:$)'
+__author__  = 'Christopher Arndt'
+__date__    = '$Date:$'
+
+
 import argparse
+import logging
 import os
 import sys
 import time
 
-from os.path import basename
+from os.path import basename, exists, isdir, join
 
 import rtmidi
 
+try:
+    raw_input
+except NameError:
+    # Python 3
+    raw_input = input
 
-def info(msg, *args):
-    sys.stderr.write(msg.format(*args) + '\n')
+
+log = logging.getLogger("sendsysex")
+SYSTEM_EXCLUSIVE = b'\xF0'
+END_OF_EXCLUSIVE = b'\xF7'
+
+
+def send_sysex_file(filename, midiout, portname, prompt=True, delay=50):
+    """Read file given by filename and send all consecutive sysex messages
+    found in it to given midiout after prompt.
+
+    """
+    bn = basename(filename)
+
+    with open(filename, 'rb') as sysex_file:
+        data = sysex_file.read()
+
+        if data.startswith(SYSTEM_EXCLUSIVE):
+            try:
+                if prompt:
+                    yn = raw_input("Send '%s' to %s (y/N)? " % (bn, portname))
+            except (EOFError, KeyboardInterrupt):
+                print('\n')
+                raise StopIteration
+
+            if not prompt or yn.lower() in ('y', 'yes'):
+                sox = 0
+                i = 0
+
+                while sox >= 0:
+                    sox = data.find(SYSTEM_EXCLUSIVE, sox)
+
+                    if sox >= 0:
+                        eox = data.find(END_OF_EXCLUSIVE, sox)
+
+                        if eox >= 0:
+                            sysex_msg = data[sox:eox+1]
+                            # Python 2: convert data into list of integers
+                            if isinstance(sysex_msg, str):
+                                sysex_msg = [ord(c) for c in sysex_msg]
+
+                            log.info("Sending '%s' message #%03i...", bn, i)
+                            midiout.send_message(sysex_msg)
+                            time.sleep(0.001 * delay)
+
+                            i += 1
+                        else:
+                            break
+
+                        sox = eox + 1
+        else:
+            log.warning("File '%s' does not start with a sysex message.", bn)
+
 
 def main(args=None):
+    """Main program function.
+
+    Parses command line (parsed via ``args`` or from ``sys.argv``), detects
+    and optionally lists MIDI output ports, opens given MIDI ouput port,
+    assmbles list of sysex files and calls ``send_sysex_file`` on each of them.
+
+    """
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(dest='sysexfiles', nargs="*", metavar="SYSEX",
-        help='MIDI system exclusive file(s) to send')
+        help='MIDI system exclusive files or directories to send.')
     parser.add_argument('-l',  '--list-ports', action="store_true",
         help='list available MIDI output ports')
     parser.add_argument('-p',  '--port', dest='port', default=0, type=int,
-        help='MIDI output port number (default: 0)')
+        help='MIDI output port number (default: %(default)s)')
     parser.add_argument('-d',  '--delay', default="50", metavar="MS", type=int,
         help='delay between sending each Sysex message in milliseconds '
-        '(default: 50)')
-    parser.add_argument('-y',  '--no-prompt', dest='prompt', 
+        '(default: %(default)s)')
+    parser.add_argument('-y',  '--no-prompt', dest='prompt',
         action="store_false", help='do not ask for confirmation before sending')
     parser.add_argument('-v',  '--verbose', action="store_true",
         help='verbose output')
 
-    args = parser.parse_args()
+    args = parser.parse_args(args if args is not None else sys.argv)
+
+    logging.basicConfig(format="%(name)s: %(levelname)s - %(message)s",
+        level=logging.DEBUG if args.verbose else logging.WARNING,)
 
     try:
         midiout = rtmidi.MidiOut()
@@ -47,65 +124,43 @@ def main(args=None):
         if ports:
             if args.list_ports:
                 for i, port in enumerate(ports):
-                    info("{}: {}", i, port)
-                
+                    print("%i: %s" % (i, port))
+
                 return 0
 
             if args.port < len(ports):
                 midiout.open_port(args.port)
-                port_name = ports[args.port]
+                portname = midiout.get_port_name(args.port)
             else:
-                info("MIDI port number out of range.")
-                info("Use '-l' to list MIDI ports.")
+                log.error("MIDI port number out of range.")
+                log.error("Use '-l' option to list MIDI ports.")
                 return 2
         else:
-            info("No MIDI output ports found.")
+            log.error("No MIDI output ports found.")
             return 1
 
-        if not args.sysexfiles:
-            args.sysexfiles = sorted([fn for fn in os.listdir(os.curdir)
-                                      if fn.lower().endswith('.syx')])
+        files = []
+        for path in args.sysexfiles or [os.curdir]:
+            if isdir(path):
+                files.extend(sorted([join(path, fn) for fn in os.listdir(path)
+                    if fn.lower().endswith('.syx')]))
+            elif exists(path):
+                files.append(path)
+            else:
+                log.error("File '%s' not found.")
 
-        
-        if not args.sysexfiles:
-            info("No sysex (.syx) files found in current directory.")
+        if not files:
+            log.warning("No sysex (.syx) files found in given directories or "
+                "working directory.")
 
-        for fn in args.sysexfiles:
-            syx = open(fn, 'rb').read()
-
-            if syx.startswith('\xF0'):
-                try:
-                    if args.prompt:
-                        yn = raw_input(
-                            "Send '{}' to {} (y/N)? ".format(basename(fn), port_name))
-                except (EOFError, KeyboardInterrupt):
-                    print()
-                    break
-
-                if not args.prompt or yn.lower() in ('y', 'yes'):
-                    sox = 0
-                    i = 0
-                    while sox >= 0:
-                        sox = syx.find('\xF0', sox)
-                        
-                        if sox >= 0:
-                            eox = syx.find('\xF7', sox)
-
-                            if eox >= 0:
-                                msg = [ord(c) for c in syx[sox:eox+1]]
-                                if args.verbose:
-                                    info("Sending '{}' message #{:03}...",
-                                        basename(fn), i)
-                                midiout.send_message(msg)
-                                i += 1
-                                time.sleep(0.001 * args.delay)
-                            else:
-                                break
-
-                            sox = eox + 1
-            elif args.verbose:
-                info("File '{}' not recognized as sysex message.", basename(fn))
-
+        for filename in files:
+            try:
+                send_sysex_file(
+                    filename, midiout, portname, args.prompt, args.delay)
+            except StopIteration:
+                break
+            except Exception as exc:
+                log.error("Error while sending file '%s': %s", (filename, exc))
     finally:
         midiout.close_port()
         del midiout
