4bc3b8f65e294af8ece101dcc6e2f9aeee73f957
carndt <carndt@d91bdeee-4e13-0410-98cf-97065b31a653>
2013-09-30 23:56:49 +0000

    Fix error when invalid port number was passed and add `use_virtual` argument.

--- projects/python-rtmidi/trunk/rtmidi/midiutil.py
+++ projects/python-rtmidi/trunk/rtmidi/midiutil.py
@@ -21,62 +21,55 @@ import rtmidi
 
 log = logging.getLogger(__name__)
 
+def _prompt_for_virtual(type_):
+    return raw_input("Do you want to create a virtual MIDI %s port? (y/N) "
+        % type_).strip().lower() in ['y', 'yes']
 
-def open_midiport(port, type_="input"):
+def open_midiport(port=None, type_="input", use_virtual=False):
     log.debug("Creating MidiIn object.")
     midiobj = rtmidi.MidiIn() if type_ == "input" else rtmidi.MidiOut()
     type_ = "input" if isinstance(midiobj, rtmidi.MidiIn) else "ouput"
 
     ports = midiobj.get_ports(encoding='latin1'
         if sys.platform.startswith('win') else 'utf-8')
-    use_virtual = False
 
     if port is None:
         try:
-            r = raw_input(
-                "Do you want to create a virtual MIDI %s port? (y/N) " % type_)
-
-            if r.strip().lower() in ['y', 'yes']:
-                use_virtual = True
+            if use_virtual or _prompt_for_virtual(type_):
                 port_name = "Virtual MIDI Input"
                 log.info("Opening virtual MIDI %s port.", type_)
                 midiobj.open_virtual_port()
+                return midiobj, port_name
         except (KeyboardInterrupt, EOFError):
             del midiobj
             print('')
             sys.exit()
 
-    if not use_virtual:
-        if not ports:
+    if len(ports) == 0:
+        del midiobj
+        raise IOError("No MIDI %s ports found." % type_)
+
+    while port is None or (port < 0 or port >= len(ports)):
+        print("Available MIDI %s ports:\n" % type_)
+
+        for portno, name in enumerate(ports):
+            print("[%i] %s" % (portno, name))
+
+        print('')
+
+        try:
+            r = raw_input("Select MIDI %s port (Control-C to exit): "
+                % type_)
+            port = int(r)
+        except (KeyboardInterrupt, EOFError):
             del midiobj
-            raise IOError("No MIDI %s ports found." % type_)
-        elif port is None or (port < 0 or port >= len(ports)):
-            print("Available MIDI %s ports:\n" % type_)
-
-            for portno, name in enumerate(ports):
-                print("[%i] %s" % (portno, name))
-
             print('')
+            sys.exit()
+        except (ValueError, TypeError):
+            port = None
 
-            while port is None:
-                try:
-                    r = raw_input("Select MIDI %s port (Control-C to exit): "
-                        % type_)
-                    port = int(r)
+    port_name = ports[port]
 
-                    if port < 0 or port >= len(ports):
-                        print("Invalid port number: %i" % port)
-                        port = None
-                except (KeyboardInterrupt, EOFError):
-                    del midiobj
-                    print('')
-                    sys.exit()
-                except (ValueError, TypeError):
-                    port = None
-
-        port_name = ports[port]
-
-        log.info("Opening MIDI %s port #%i (%s)." % (type_, port, port_name))
-        midiobj.open_port(port)
-
+    log.info("Opening MIDI %s port #%i (%s)." % (type_, port, port_name))
+    midiobj.open_port(port)
     return midiobj, port_name
