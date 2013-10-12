38aed53d3ff956512b21470fd60b9d41b03d0f3d
carndt <carndt@d91bdeee-4e13-0410-98cf-97065b31a653>
2013-10-12 02:13:36 +0000

    Beginn conversion of osc2midi main script to use oscdispatcher

--- projects/python-rtmidi/trunk/examples/osc2midi/main.py
+++ projects/python-rtmidi/trunk/examples/osc2midi/main.py
@@ -6,7 +6,7 @@
 """Simple uni-directional OSC to MIDI gateway."""
 
 __program__ = 'oscmidi.py'
-__version__ = '1.1 ($Rev$)'
+__version__ = '1.2 ($Rev$)'
 __author__  = 'Christopher Arndt'
 __date__    = '$Date$'
 __usage__   = "%(prog)s [-d DEVICE] [-p PORT]"
@@ -24,6 +24,7 @@ from rtmidi.midiconstants import *
 
 from .midiio import MidiOutputProc, MidiOutputThread
 from .midievents import *
+from .oscdispatcher import OSCDispatcher
 
 try:
     raw_input
@@ -39,17 +40,101 @@ except NameError:
 log = logging.getLogger("osc2midi")
 
 
+# These patterns will work with the TouchOSC Midi16 control layout
+patterns = (
+    # general
+    (r'/(?P<page>\w+)$', '', 'page_change'),
 
-class OSC2MIDI(liblo.ServerThread):
-    def __init__(self, midiout, port=5555):
-        super(OSC2MIDI, self).__init__(port)
-        log.info("Listening on URL: " + self.get_url())
+    # page 1
+    (r'/1/fader1$', 'f', 'sendcc', dict(cc=MODULATION_WHEEL, channel=1)),
+    (r'/1/fader2$', 'f', 'sendcc', dict(cc=DATA_ENTRY, channel=1)),
+    (r'/1/fader3$', 'f', 'sendcc', dict(cc=13, channel=1)),
+    (r'/1/fader4$', 'f', 'sendcc', dict(cc=CHANNEL_VOLUME, channel=1)),
+
+    (r'/1/toggle1$', 'f', 'sendcc', dict(cc=SUSTAIN_ONOFF, channel=1)),
+    (r'/1/toggle2$', 'f', 'sendcc', dict(cc=PORTAMENTO_ONOFF, channel=1)),
+    (r'/1/toggle3$', 'f', 'sendcc', dict(cc=LEGATO_ONOFF, channel=1)),
+
+    (r'/1/push(?P<i:note>\d+)$', 'f', 'noteonoff', dict(transpose=60, channel=1)),
+
+    (r'/1/xy$', 'ff', 'sendtwocc',
+        dict(cc1=EXPRESSION_CONTROLLER, cc2=PAN, channel=1)),
+
+    # page 2, map control number to MIDI channel
+    (r'/2/fader(?P<i:channel>\d+)$', 'f', 'sendcc', dict(cc=CHANNEL_VOLUME)),
+    (r'/2/toggle(?P<i:channel>\d+)$', 'f', 'sendcc', dict(cc=SUSTAIN_ONOFF)),
+
+    # page 3, the same, differnt controllers
+    (r'/3/fader(?P<i:channel>\d+)$', 'f', 'sendcc', dict(cc=PAN)),
+    (r'/3/toggle(?P<i:channel>\d+)$', 'f', 'sendcc', dict(cc=29)),
+
+    ## catch-all pattern that maps page number to MIDI channel
+    ## and control number to MIDI controller number, e.g.
+    ##
+    ##    /1/push64 1.0 --> MIDI CC #64, channel 1, value 127
+    ##
+    #(r'/(?P<i:channel>\d+)/(fader|push|rotary|toggle)(?P<i:cc>\d+)$', 'f',
+    #    'sendcc'),
+
+    #(r'/(?P<i:page>\d+)/multitoggle(?P<i:cc>\d+)/(?P<i:col>\d+)/(?P<i:row>\d+)$',
+    #    'f', 'sendcc'),
+    #(r'/(?P<i:page>\d+)/xy(?P<i:cc>\d+)$', 'ff', 'sendtwocc')
+)
+
+
+class OSC2MIDIHandler(object):
+    def __init__(self, midiout):
         self.midiout = midiout
         self._note_state = [{}] * 16
         self._program = [0] * 16
         #self._velocity = {}
 
-    @liblo.make_method(None, None)
+    def sendcc(self, value, cc=0, channel=1, **kwargs):
+        self.midiout.send(
+            MidiEvent.fromdata(CONTROLLER_CHANGE,
+                channel=(channel-1) & 0x7f,
+                data=[cc & 0x7f, int(127 * value) & 0x7f]))
+
+    def sendtwocc(self, val1, val2, cc1=0, cc2=32, channel=1, **kwargs):
+        self.midiout.send(
+            MidiEvent.fromdata(CONTROLLER_CHANGE,
+                channel=(channel-1) & 0x7f,
+                data=[cc1 & 0x7f, int(127 * val1) & 0x7f]))
+        self.midiout.send(
+            MidiEvent.fromdata(CONTROLLER_CHANGE,
+                channel=(channel-1) & 0x7f,
+                data=[cc2 & 0x7f, int(127 * val2) & 0x7f]))
+
+    def page_change(self, page=None):
+        log.info("Page %s selected.", page)
+
+    def noteonoff(self, val, note=60, channel=1, velocity=None,
+            transpose=0,**kwargs):
+        note += transpose
+
+        if val:
+            velocity = velocity or 100
+            self.midiout.send(
+                MidiEvent.fromdata(NOTE_ON,
+                    channel=(channel-1) & 0x7f,
+                    data=[note & 0x7f, velocity & 0x7f]))
+            self._note_state[channel][note] = velocity
+        else:
+            if velocity is None:
+                try:
+                    velocity = self._note_state[channel][note]
+                    if velocity is None:
+                        raise ValueError
+                except KeyError, ValueError:
+                    velocity = 0
+
+            self.midiout.send(
+                MidiEvent.fromdata(NOTE_OFF,
+                    channel=(channel-1) & 0x7f,
+                    data=[note & 0x7f, velocity & 0x7f]))
+            self._note_state[channel][note] = None
+
+    """
     def _osc_callback(self, path, args, types, source, data=None):
         log.debug("OSC recv: @%0.6f %s,%s %r", time.time(), path, types, args)
         try:
@@ -66,13 +151,7 @@ class OSC2MIDI(liblo.ServerThread):
             return 1
 
         try:
-            if msgtype == 'cc':
-                self.midiout.send(
-                    MidiEvent.fromdata(CONTROLLER_CHANGE,
-                        channel=int(channel) & 0x7f,
-                        data=[int(data1) & 0x7f, args[0] & 0x7f]))
-
-            elif msgtype == 'on':
+            if msgtype == 'on':
                 channel = int(channel) & 0xf
                 note = int(data1) & 0x7f
 
@@ -139,6 +218,18 @@ class OSC2MIDI(liblo.ServerThread):
         except StandardError:
             import traceback
             traceback.print_exc()
+    """
+
+
+class OSC2MIDIServer(liblo.ServerThread):
+    def __init__(self, midiout, port=5555):
+        super(OSC2MIDIServer, self).__init__(port)
+        log.info("Listening on URL: " + self.get_url())
+        osc2midi = OSC2MIDIHandler(midiout)
+        dispatcher = OSCDispatcher(patterns, search_ns=osc2midi, cache_size=512)
+        log.info("Registering OSC method handler.")
+        self.add_method(None, None, dispatcher.dispatch)
+
 
 def select_midiport(midi, default=0):
     type_ = "input" if isinstance(midi, rtmidi.MidiIn) else "output"
@@ -207,7 +298,7 @@ def main(args=None):
     else:
         midiout = MidiOutputProc(name=__program__, port=args.midiport)
 
-    server = OSC2MIDI(midiout, args.oscport)
+    server = OSC2MIDIServer(midiout, args.oscport)
 
     print("Entering main loop. Press Control-C to exit.")
     try:
