c7c287983a9762241d14f5165da70383d262827c
carndt <carndt@d91bdeee-4e13-0410-98cf-97065b31a653>
2013-10-15 10:31:36 +0000

    Allow to pass port as (a substring of) a port name; re-raise `EOFError` and `KeyboardInterrupt` instead of using `sys.exit`; allow to pass client and port name; add `interactive` keyword argument (default `True`) to disable interactive prompt for port

--- projects/python-rtmidi/trunk/rtmidi/midiutil.py
+++ projects/python-rtmidi/trunk/rtmidi/midiutil.py
@@ -15,7 +15,7 @@ try:
 except NameError:
     # Python 3
     raw_input = input
-
+    basestring = str
 
 import rtmidi
 
@@ -25,9 +25,11 @@ def _prompt_for_virtual(type_):
     return raw_input("Do you want to create a virtual MIDI %s port? (y/N) "
         % type_).strip().lower() in ['y', 'yes']
 
-def open_midiport(port=None, type_="input", use_virtual=False):
+def open_midiport(port=None, type_="input", use_virtual=False,
+        interactive=True, client_name=None, port_name=None):
     log.debug("Creating MidiIn object.")
-    midiobj = rtmidi.MidiIn() if type_ == "input" else rtmidi.MidiOut()
+    midiobj = (rtmidi.MidiIn(name=client_name)
+        if type_ == "input" else rtmidi.MidiOut(name=client_name))
     type_ = "input" if isinstance(midiobj, rtmidi.MidiIn) else "ouput"
 
     ports = midiobj.get_ports(encoding='latin1'
@@ -35,21 +37,35 @@ def open_midiport(port=None, type_="input", use_virtual=False):
 
     if port is None:
         try:
-            if use_virtual or _prompt_for_virtual(type_):
-                port_name = "Virtual MIDI Input"
+            if use_virtual or (interactive and _prompt_for_virtual(type_)):
+                if not port_name:
+                    port_name = "Virtual MIDI Input"
                 log.info("Opening virtual MIDI %s port.", type_)
-                midiobj.open_virtual_port()
+                midiobj.open_virtual_port(port_name)
                 return midiobj, port_name
         except (KeyboardInterrupt, EOFError):
             del midiobj
             print('')
-            sys.exit()
+            raise
 
     if len(ports) == 0:
         del midiobj
         raise IOError("No MIDI %s ports found." % type_)
 
-    while port is None or (port < 0 or port >= len(ports)):
+    try:
+        port = int(port)
+    except (TypeError, ValueError):
+        if isinstance(port, basestring):
+            portspec = port
+            for portno, name in enumerate(ports):
+                if portspec in name:
+                    port = portno
+                    break
+            else:
+                print("No port matching '%s' found." % portspec)
+                port = None
+
+    while interactive and port is None or (port < 0 or port >= len(ports)):
         print("Available MIDI %s ports:\n" % type_)
 
         for portno, name in enumerate(ports):
@@ -58,18 +74,21 @@ def open_midiport(port=None, type_="input", use_virtual=False):
         print('')
 
         try:
-            r = raw_input("Select MIDI %s port (Control-C to exit): "
-                % type_)
+            r = raw_input("Select MIDI %s port (Control-C to exit): " % type_)
             port = int(r)
         except (KeyboardInterrupt, EOFError):
             del midiobj
             print('')
-            sys.exit()
+            raise
         except (ValueError, TypeError):
             port = None
 
-    port_name = ports[port]
+    if port is not None and (port > 0 and port <= len(ports)):
+        if not port_name:
+            port_name = ports[port]
 
-    log.info("Opening MIDI %s port #%i (%s)." % (type_, port, port_name))
-    midiobj.open_port(port)
-    return midiobj, port_name
+        log.info("Opening MIDI %s port #%i (%s)." % (type_, port, port_name))
+        midiobj.open_port(port, port_name)
+        return midiobj, port_name
+    else:
+        raise ValueError("Invalid port.")
