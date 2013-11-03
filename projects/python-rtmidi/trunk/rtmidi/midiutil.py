63a5dfc469ddc345d186cdd96df71c23519b8a87
carndt <carndt@d91bdeee-4e13-0410-98cf-97065b31a653>
2013-11-03 20:07:18 +0000

    Add docstring for `open_midiport` function.

--- projects/python-rtmidi/trunk/rtmidi/midiutil.py
+++ projects/python-rtmidi/trunk/rtmidi/midiutil.py
@@ -2,6 +2,11 @@
 #
 # util.py
 #
+"""Collection of utility functions for handling MIDI I/O and ports.
+
+Currently only contains one public function, ``open_midiport``.
+
+"""
 
 __all__ = [
     'open_midiport'
@@ -19,14 +24,81 @@ except NameError:
 
 import rtmidi
 
+
 log = logging.getLogger(__name__)
 
+
 def _prompt_for_virtual(type_):
+    """Prompt on the console whether a virtual MIDI port should be opened."""
+
     return raw_input("Do you want to create a virtual MIDI %s port? (y/N) "
         % type_).strip().lower() in ['y', 'yes']
 
 def open_midiport(port=None, type_="input", use_virtual=False,
         interactive=True, client_name=None, port_name=None):
+    """Open MIDI port for input or output and return MidiIn/MidiOut instance.
+
+    Arguments:
+
+    ``port``
+        A MIDI port number or (substring of) a port name or ``None``.
+
+        Available ports are enumerated starting from zero separately for input
+        and output ports. If onyl a substring of a port name is given, the
+        first matching port is used.
+
+    ``type_``
+        Must be ``"input"`` or ``"output"``. Determines whether a ``MidiIn``
+        or ``MidiOut`` instance will be created and returned.
+
+    ``use_virtual``
+        If ``port is ``None``, should a virtual MIDI port be opened? Defaults
+        to ``False``.
+
+    ``interactive``
+        If port is ``None`` or no MIDI port matching the port number or name is
+        available, should the user be prompted on the console whether to open
+        a virtual MIDI port (if ``use_virtual`` is ``True``) and/or with a list
+        of available MIDI ports and the option to choose one? Defaults to
+        `` True``.
+
+    ``client_name``
+        The name of the MIDI client passed when instantiating a `MidiIn`` or
+        ``MidiOut`` object.
+
+        See the documentation of the constructor for these classes for the
+        default values and caveats and OS-dependant ideosyncracies regarding
+        the name.
+
+    ``port_name``
+        The name of the MIDI port passed to the ``open_port`` or
+        ``open_virtual_port`` method of the new ``MidiIn`` or ``MidiOut``
+        instance.
+
+        See the documentation of the ``open_port`` resp. ``open_virtual_port``
+        methods for the default values and caveats when wanting to change the
+        port name afterwards.
+
+    Returns:
+
+    A two-element tuple of a new ``MidiIn`` or ``MidiOut`` instance and the
+    name of the MIDI port which was opened.
+
+    Exceptions:
+
+    ``KeyboardInterrupt, EOFError``
+        Raised when the user presses Control-C or Control-D during a console
+        prompt.
+
+    ``IOError``
+        Raised when no MIDI input or output ports (depending on what was
+        requested) are available.
+
+    ``ValueError``
+        Raised when an invalid port number or name is passed and
+        ``interactive`` is ``False``.
+
+    """
     log.debug("Creating MidiIn object.")
     midiobj = (rtmidi.MidiIn(name=client_name)
         if type_ == "input" else rtmidi.MidiOut(name=client_name))
