a29cc579682a79e38fa4a2538aa89e1a5b6a4dc4
Christopher Arndt <SpotlightKid@users.noreply.github.com>
2014-03-23 09:08:58 +0000

    - Send note off at start of each step if note is active - Enhance pattern syntax to allow continued notes - Update examples and README for the above two changes - Move `activate_drumkit` method to `Sequencer` class - Re-order init arguments of `Sequencer` class and add `volume` argument - Set MIDI channel volume in `Sequencer.run` method - No default for drumkit option and fix `activate_drumkit` to handle it

--- examples/drumseq/drumseq.py
+++ examples/drumseq/drumseq.py
@@ -17,30 +17,34 @@ from rtmidi.midiutil import open_midiport
 from rtmidi.midiconstants import *
 
 FUNKYDRUMMER = """
-    #  ,...,...,...,...
-    42 xxxxx-x-xxxxx-xx Closed Hi-hat
-    46 -----x-x-----x-- Open Hi-hat
-    38 ----x--m-m-mx--m Snare
-    36 x-x-------x--x-- Bassdrum
+    #  |...|...|...|...
+    42 xxxxx.x.xxxxx.xx Closed Hi-hat
+    46 .....x.x.....x.. Open Hi-hat
+    38 ....x..m.m.mx..m Snare
+    36 x.x.......x..x.. Bassdrum
 """
 
 
 class Sequencer(threading.Thread):
     """MIDI output and scheduling thread."""
 
-    def __init__(self, midiout, bpm, pattern, channel=9):
+    def __init__(self, midiout, pattern, bpm, channel=9, volume=127):
         super(Sequencer, self).__init__()
         self.midiout = midiout
         self.bpm = max(20, min(bpm, 400))
         self.interval = 15. / self.bpm
         self.pattern = pattern
         self.channel = channel
+        self.volume = volume
         self.start()
 
     def run(self):
         self.done = False
         self.callcount = 0
-        self.pattern.activate_drumkit(channel=self.channel)
+        self.activate_drumkit(self.pattern.kit)
+        cc = CONTROL_CHANGE | self.channel
+        midiout.send_message([cc, CHANNEL_VOLUME, self.volume & 0x7F])
+
         # give MIDI instrument some time to activate drumkit
         sleep(0.3)
         self.started = timenow()
@@ -54,7 +58,7 @@ class Sequencer(threading.Thread):
             timetowait = nexttime - timenow()
             sleep(timetowait)
 
-        midiout.send_message([CONTROL_CHANGE | self.channel, ALL_SOUND_OFF, 0])
+        self.midiout.send_message([cc, ALL_SOUND_OFF, 0])
 
     def worker(self):
         """Variable time worker function.
@@ -64,16 +68,33 @@ class Sequencer(threading.Thread):
         """
         self.pattern.playstep(self.midiout, self.channel)
 
+    def activate_drumkit(self, kit):
+        if isinstance(kit, (list, tuple)):
+            msb, lsb, pc = kit
+        elif kit is not None:
+            msb = lsb = None
+            pc = kit
+
+        cc = CONTROL_CHANGE | self.channel
+        if msb is not None:
+            self.midiout.send_message([cc, BANK_SELECT_MSB, msb & 0x7F])
+
+        if lsb is not None:
+            self.midiout.send_message([cc, BANK_SELECT_LSB, lsb & 0x7F])
+
+        if kit is not None and pc is not None:
+            self.midiout.send_message([PROGRAM_CHANGE | self.channel, pc & 0x7F])
+
 
 class Drumpattern(object):
     """Container and iterator for a multi-track step sequence."""
 
     velocities = {
-        "-": 0,   # off
-        ".": 32,  # low
-        "s": 60,  # soft
-        "m": 100, # medium
-        "x": 120, # hard
+        "-": None, # continue note
+        ".": 0,    # off
+        "s": 60,   # soft
+        "m": 100,  # medium
+        "x": 120,  # hard
         }
 
     def __init__(self, pattern, kit=0):
@@ -93,46 +114,28 @@ class Drumpattern(object):
                 self.steps = len(strokes)
 
         self.step = 0
+        self._notes = {}
 
     def reset(self):
         self.step = 0
 
     def playstep(self, midiout, channel=9):
-        for ins in self.instruments:
-            patch, strokes = ins
+        for note, strokes in self.instruments:
             char = strokes[self.step]
-            velocity = Drumpattern.velocities.get(char, 64)
+            velocity = self.velocities.get(char)
 
-            if velocity:
-                note_on = [NOTE_ON | channel, patch, velocity]
-                midiout.send_message(note_on)
+            if velocity is not None:
+                if self._notes.get(note):
+                    midiout.send_message([NOTE_ON | channel, note, 0])
+                    self._notes[note] = 0
+                if velocity > 0:
+                    midiout.send_message([NOTE_ON | channel, note, velocity])
+                    self._notes[note] = velocity
 
         self.step += 1
 
         if self.step >= self.steps:
             self.step = 0
-
-    def activate_drumkit(self, kit=None, volume=127, channel=9):
-        if kit is not None:
-            self.kit = kit
-
-        if isinstance(self.kit, (list, tuple)):
-            msb, lsb, pc = self.kit
-        elif self.kit is not None:
-            msb = lsb = None
-            pc = self.kit
-
-        cc = CONTROL_CHANGE | channel
-        if msb is not None:
-            midiout.send_message([cc, BANK_SELECT_MSB, msb & 0x7F])
-
-        if lsb is not None:
-            midiout.send_message([cc, BANK_SELECT_LSB, lsb & 0x7F])
-
-        if pc is not None:
-            midiout.send_message([PROGRAM_CHANGE | channel, pc & 0x7F])
-
-        midiout.send_message([cc, CHANNEL_VOLUME, volume & 0x7F])
 
 
 if __name__ == "__main__":
@@ -146,8 +149,8 @@ if __name__ == "__main__":
         help="MIDI channel (default: %(default)s)")
     ap.add_argument('-p', '--port',
         help="MIDI output port number (default: ask)")
-    ap.add_argument('-k', '--kit', type=int, default=0, metavar='KIT',
-        help="Drum kit MIDI program number (default: %(default)s)")
+    ap.add_argument('-k', '--kit', type=int, metavar='KIT',
+        help="Drum kit MIDI program number (default: none)")
     ap.add_argument('--bank-msb', type=int, metavar='MSB',
         help="MIDI bank select MSB (CC#00) number (default: none)")
     ap.add_argument('--bank-lsb', type=int, metavar='MSB',
@@ -170,7 +173,7 @@ if __name__ == "__main__":
     except (EOFError, KeyboardInterrupt):
         sys.exit()
 
-    seq = Sequencer(midiout, args.bpm, pattern, args.channel-1)
+    seq = Sequencer(midiout, pattern, args.bpm, args.channel-1)
 
     print("Playing drum loop at %i BPM, press Control-C to quit." % seq.bpm)
 
