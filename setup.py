c90735a1f532c95784880a0b24ff23108684026c
Christopher Arndt <SpotlightKid@users.noreply.github.com>
2015-05-21 12:10:42 +0000

    Cleaned up old compatibility stuff in setup.py; use cythonize

--- setup.py
+++ setup.py
@@ -7,11 +7,10 @@ import sys
 from ctypes.util import find_library
 from os.path import exists, join
 
-from setuptools import setup
-
-from distutils.extension import Extension
-from distutils.version import LooseVersion as V
+from setuptools import setup  # needs to stay before the imports below!
 from distutils.dist import DistributionMetadata
+from distutils.extension import Extension
+
 
 # For compiling python-rtmidi for Windows, get Microsoft Visual Studio
 # Express (for Python <= 3.2 get the 2008 Edition, for Python >= 3.3 get
@@ -20,7 +19,7 @@ from distutils.dist import DistributionMetadata
 WINLIB_DIR = r'C:\Program Files\Microsoft SDKs\Windows\v6.0A\Lib'
 
 # Suport for Windows Kernel Streaming API was removed, so the WININC_DIR
-# setting is currently not needed.
+# setting below is currently not needed and therefor commented out.
 ## Also adapt the following path to the directory containing the Microsoft
 ## SDK headers or copy 'ks.h' and 'ksmedia.h' to the 'src' directory.
 #WININC_DIR = r'C:\Program Files\Microsoft SDKs\Windows\v6.0A\Include'
@@ -37,59 +36,29 @@ setup_opts = {}
 release_info = join(PKG_DIR, 'release.py')
 exec(compile(open(release_info).read(), release_info, 'exec'), {}, setup_opts)
 
-# test for required Cython version
-try:
-    import Cython
-    cython_ver = V(Cython.__version__)
-    if cython_ver <= V('0.17'):
-        raise ImportError("Cython version %s is too old." % cython_ver)
-
-    from Cython.Distutils import build_ext
-    setup_opts['cmdclass'] = {'build_ext': build_ext}
-    sources = [join(SRC_DIR, "_rtmidi.pyx"), join(SRC_DIR, "RtMidi.cpp")]
-except ImportError:
-    if not exists(join(SRC_DIR, "_rtmidi.cpp")):
-        print("""\
-Could not import Cython or the version found is too old.
-
-Cython >= 0.17 is required to compile '_rtmidi.pyx' into '_rtmidi.cpp'.
-
-Install Cython from https://pypi.python.org/pypi/Cython or use the precompiled
-'_rtmidi.cpp' file from the python-rtmidi source distribution.""")
-        sys.exit(1)
-
-    sources = [join(SRC_DIR, "_rtmidi.cpp"), join(SRC_DIR, "RtMidi.cpp")]
-
 # Add our own custom distutils command to create *.rst files from templates
 # Template files are listed in setup.cfg
 from fill_template import FillTemplate
 
 setup_opts.setdefault('cmdclass', {})['filltmpl'] = FillTemplate
 
-# Monkey-patch the class used by the setuptools 'egg_info' command, so
-# is does not collect files through VC plugins, because the package file
-# finder function of setuptools.svn_utils is not Python 3 compatible.
-#
-# https://bitbucket.org/pypa/setuptools/issue/99/egg_info-comand-fails-parsing-manifest
-import setuptools.command.egg_info
-import setuptools.command.sdist
-
-class manifest_maker_novc(setuptools.command.egg_info.manifest_maker):
-    def add_defaults(self):
-        setuptools.command.sdist.sdist.add_defaults(self)
-        self.filelist.append(self.template)
-        self.filelist.append(self.manifest)
-
-        if exists(self.manifest):
-            self.read_manifest()
-
-        ei_cmd = self.get_finalized_command('egg_info')
-        self.filelist.include_pattern("*", prefix=ei_cmd.egg_info)
-
-setuptools.command.egg_info.manifest_maker = manifest_maker_novc
-# end of hack
-
 # Set up options for compiling the _rtmidi Extension
+try:
+    from Cython.Build import cythonize
+    sources = [join(SRC_DIR, "_rtmidi.pyx"), join(SRC_DIR, "RtMidi.cpp")]
+except ImportError:
+    if not exists(join(SRC_DIR, "_rtmidi.cpp")):
+        print("""\
+Could not import Cython. Cython >= 0.17 is required to compile the Cython
+source into the C++ source.
+
+Install Cython from https://pypi.python.org/pypi/Cython or use the precompiled
+'_rtmidi.cpp' file from the python-rtmidi source distribution.""")
+        sys.exit(1)
+
+    cythonize = lambda x: x
+    sources = [join(SRC_DIR, "_rtmidi.cpp"), join(SRC_DIR, "RtMidi.cpp")]
+
 define_macros = [('__PYX_FORCE_INIT_THREADS', None)]
 include_dirs = [SRC_DIR]
 libraries = []
@@ -103,19 +72,19 @@ jack = True
 winks = False
 winmm = True
 
-if '--no-alsa' in sys.argv[1:]
+if '--no-alsa' in sys.argv:
     alsa = False
     sys.argv.remove('--no-alsa')
 
-if '--no-coremidi' in sys.argv[1:]
+if '--no-coremidi' in sys.argv:
     coremidi = False
     sys.argv.remove('--no-coremidi')
 
-if '--no-jack' in sys.argv[1:]
+if '--no-jack' in sys.argv:
     jack = False
     sys.argv.remove('--no-jack')
 
-if '--no-winmm' in sys.argv[1:]:
+if '--no-winmm' in sys.argv:
     winmm = False
     sys.argv.remove('--no-winmm')
 
@@ -189,7 +158,7 @@ extensions = [
 setup(
     packages = ['rtmidi', 'osc2midi'],
     package_dir = {'osc2midi': 'examples/osc2midi'},
-    ext_modules = extensions,
+    ext_modules = cythonize(extensions),
     extras_require = {
         'osc2midi':  ['pyliblo', 'PyYAML'],
     },
