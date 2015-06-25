e22b6e0774e0b6ce882ceb78f1a6c1e87387a004
Christopher Arndt <chris@chrisarndt.de>
2015-06-25 06:21:10 +0200

    Add unit test stubs

---
+++ tests/test_rtmidi.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Unit tests for the rtmidi module."""
+
+import unittest
+
+import rtmidi
+
+
+class TestRtMidi(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
