ddef664ff6e45331b6a56a574e305c6288ae5db7
Christopher Arndt <chris@chrisarndt.de>
2015-11-15 02:48:06 +0100

    Optimize loop

--- examples/sequencer/sequencer.py
+++ examples/sequencer/sequencer.py
@@ -51,22 +51,24 @@ class MidiEvent(object):
 class SequencerThread(threading.Thread):
     def __init__(self, midiout, queue=None, bpm=120.0, ppqn=480):
         super(SequencerThread, self).__init__()
-        log.debug("Created sequencer thread.")
+        # log.debug("Created sequencer thread.")
         self.midiout = midiout
 
         # inter-thread communication
         self.queue = queue
         if queue is None:
             self.queue = deque()
-            log.debug("Created queue for MIDI output.")
+            # log.debug("Created queue for MIDI output.")
 
         self._stopped = threading.Event()
         self._finished = threading.Event()
 
-        # Set to current tiemm when seqquence is started
-        self._start_time = None
+        # Set to current time when sequence is started
+        self._starttime = None
         # Counts elapsed ticks when sequence is running
         self._tickcnt = None
+        # Max number of input queue events to get in one loop
+        self._batchsize = 100
 
         # run-time options
         self.ppqn = ppqn
@@ -81,16 +83,16 @@ class SequencerThread(threading.Thread):
     def bpm(self, value):
         self._bpm = value
         self._tick = 60. / value / self.ppqn
-        log.debug("Changed BPM => %s, tick interval %.2f ms.",
-                  self._bpm, self._tick * 1000)
+        # log.debug("Changed BPM => %s, tick interval %.2f ms.",
+        #           self._bpm, self._tick * 1000)
 
-        if self._start_time:
-            self._start_time = time.time() - self._tickcnt * self._tick
+        if self._starttime:
+            self._starttime = time.time() - self._tickcnt * self._tick
 
     def stop(self, timeout=5):
         """Set thread stop event, causing it to exit its mainloop."""
         self._stopped.set()
-        log.debug("SequencerThread stop event set.")
+        # log.debug("SequencerThread stop event set.")
 
         if self.is_alive():
             self._finished.wait(timeout)
@@ -138,82 +140,79 @@ class SequencerThread(threading.Thread):
         # busy loop to wait for time when next batch of events needs to
         # be written to output
         pending = []
-        jitter = []
+        jitter = 0.
+        jitter_interval = self.ppqn * 4
         self._tickcnt = 0
-        self._start_time = time.time()
+        self._starttime = time.time()
 
         try:
             while not self._stopped.is_set():
-                q_events = []
                 due = []
-                current_time = time.time()
-                deadline = current_time + self._tick / 2
+                curtime = time.time()
+                deadline = curtime + self._tick / 2
 
-                # Pop events of the input queue until there are no more
-                while True:
-                    evt = self.get_event()
-
-                    if evt:
-                        q_events.append(evt)
-                    else:
-                        break
-
-
-                # Pop events of the pending queue if the are due for this tick
+                # Pop events off the pending queue
+                # if they are due for this tick
                 while True:
                     if pending:
-                        evtdue = (self._start_time +
-                                  pending[0].ticks * self._tick)
+                        evtdue = (self._starttime + pending[0].ticks *
+                                  self._tick)
 
                     if not pending or evtdue > deadline:
                         break
 
                     evt = heappop(pending)
                     heappush(due, evt)
-                    log.debug("Queued pending event for output: %r", evt)
-                    jitter.append(abs(current_time - evtdue))
+                    # log.debug("Queued pending event for output: %r", evt)
+                    jitter += abs(curtime - evtdue)
 
-                for q_event in q_events:
-                    log.debug("Got event from input queue: %r", q_event)
+                # Pop up to events off the input queue
+                for i in range(self._batchsize):
+                    evt = self.get_event()
+
+                    if not evt:
+                        break
+
+                    # log.debug("Got event from input queue: %r", evt)
                     # Check whether event should be sent out immediately
                     # or needs to be scheduled
 
-                    evtdue = self._start_time + q_event.ticks * self._tick
+                    evtdue = self._starttime + evt.ticks * self._tick
 
                     if evtdue <= deadline:
-                        heappush(due, q_event)
-                        log.debug("Queued event for output.")
-                        jitter.append(abs(current_time - evtdue))
+                        heappush(due, evt)
+                        # log.debug("Queued event for output.")
+                        jitter += abs(curtime - evtdue)
                     else:
-                        heappush(pending, q_event)
-                        log.debug("Scheduled event in pending queue.")
+                        heappush(pending, evt)
+                        # log.debug("Scheduled event in pending queue.")
 
                 # If this batch contains any due events,
                 # send them to the MIDI output.
                 if due:
                     for i in range(len(due)):
                         message = heappop(due).message
-                        log.debug("Midi Out: %r", message)
+                        # log.debug("Midi Out: %r", message)
                         self.midiout.send_message(message)
 
                 # calculate jitter
-                if len(jitter) >= 100:
-                    log.debug("Jitter (over 100 events): %0.3f (max: %0.3f,"
-                        " min: %0.3f)", sum(jitter) / len(jitter),
-                        max(jitter), min(jitter))
-                    jitter = []
+                if self._tickcnt and not self._tickcnt % jitter_interval:
+                    # log.debug("Avg. jitter (over %i ticks): %0.3f",
+                    #           jitter_interval, jitter / self._tickcnt)
+                    jitter = 0.
 
                 # loop speed adjustment
-                elapsed = time.time() - current_time
+                elapsed = time.time() - curtime
 
                 if elapsed < self._tick:
                     time.sleep(self._tick - elapsed)
 
                 self._tickcnt += 1
         except KeyboardInterrupt:
-            log.debug("KeyboardInterrupt / INT signal received.")
+            # log.debug("KeyboardInterrupt / INT signal received.")
+            pass
 
-        log.debug("Midi output mainloop exited.")
+        # log.debug("Midi output mainloop exited.")
         self._finished.set()
 
 
