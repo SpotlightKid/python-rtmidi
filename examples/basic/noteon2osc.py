22c58f7e47cdbfa0314d33c1667af6df2bd7fa5a
Christopher Arndt <chris@chrisarndt.de>
2016-05-04 19:33:08 +0200

    Add noteon2osc example script
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

---
+++ examples/basic/noteon2osc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# noteon2osc.py
+#
+"""Send an OSC message when a MIDI Note On message is received."""
+
+import time
+import liblo
+from rtmidi.midiconstants import NOTE_ON
+from rtmidi.midiutil import open_midiinput
+
+
+def midiin_callback(event, data=None):
+    message, deltatime = event
+
+    if message[0] & 0xF0 == NOTE_ON:
+        status, note, velocity = message
+        channel = (status & 0xF) + 1
+        liblo.send(
+            ('localhost', 9001),
+            '/midi/%i/noteon' % channel,
+            note,
+            velocity)
+
+
+# Prompts user for MIDI input port, defaulting to ALSA on Linux
+try:
+    port = sys.argv[1] if len(sys.argv) > 1 else None
+
+    with open_midiinput(port, client_name='noteon2osc')[0] as midiin:
+        midiin.set_callback(midiin_callback)
+
+        while True:
+            time.sleep(1)
+except (EOFError, KeyboardInterrupt):
+    print("Bye.")
