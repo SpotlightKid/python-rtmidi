4f43bb5a5702fc32751a663ef69e856fc8dff531
aib <aibok42@gmail.com>
2016-09-14 13:53:29 +0300

    Fall back to using mock if unittest.mock does not exist

--- tests/test_errorcallback.py
+++ tests/test_errorcallback.py
@@ -3,7 +3,10 @@
 """Tests for the error callback"""
 
 import unittest
-import unittest.mock
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 
 import rtmidi
 
@@ -19,19 +22,19 @@ class TestErrorCallback(unittest.TestCase):
         self.midi_in = rtmidi.MidiIn()
 
     def test_midiout_error_callback(self):
-        errcb = unittest.mock.Mock()
+        errcb = mock.Mock()
         self.midi_out.set_error_callback(errcb, self.MIDI_OUT_ERROR_USER_DATA)
         self.midi_out.open_port(self.INVALID_PORT_NUMBER)
         errcb.assert_called_with(rtmidi.ERRORTYPE_INVALID_PARAMETER,
-                                 unittest.mock.ANY,
+                                 mock.ANY,
                                  self.MIDI_OUT_ERROR_USER_DATA)
 
     def test_midiin_error_callback(self):
-        errcb = unittest.mock.Mock()
+        errcb = mock.Mock()
         self.midi_in.set_error_callback(errcb, self.MIDI_IN_ERROR_USER_DATA)
         self.midi_in.open_port(self.INVALID_PORT_NUMBER)
         errcb.assert_called_with(rtmidi.ERRORTYPE_INVALID_PARAMETER,
-                                 unittest.mock.ANY,
+                                 mock.ANY,
                                  self.MIDI_IN_ERROR_USER_DATA)
 
 
