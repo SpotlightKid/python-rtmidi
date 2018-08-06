079fce429d78198f852580af38bc0d8e40b968e9
Christopher Arndt <chris@chrisarndt.de>
2018-08-06 15:22:22 +0200

    Update copyright year in docs
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = meta['name']
-copyright = u'2012 - 2016, %s' % meta['author']
+copyright = u'2012 - 2018, %s' % meta['author']
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
