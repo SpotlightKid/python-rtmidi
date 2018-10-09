83be311061ebb788d11ad45ca7322d9453661808
Christopher Arndt <chris@chrisarndt.de>
2018-10-09 20:36:52 +0200

    Bump version number again (RC2) to test CI
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- rtmidi/release.py
+++ rtmidi/release.py
@@ -59,7 +59,7 @@ The documentation_ provides installation instructions and an API reference.
 """
 
 name = 'python-rtmidi'
-version = '1.1.2rc1'
+version = '1.1.2rc2'
 description = __doc__.splitlines()
 keywords = 'rtmidi, midi, music'
 author = 'Christopher Arndt'
