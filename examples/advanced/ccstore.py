2d26412c177ebbc4caef5629c685a5180eaf3c1b
Christopher Arndt <chris@chrisarndt.de>
2020-05-11 19:02:19 +0200

    Add new example script 'ccstore.py'
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

---
+++ examples/advanced/ccstore.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+"""Record last seen value of specific Control Change events.
+
+The main loop prints out last seen value of specific Control Change events
+every second. The  control change events are received by a MIDI input
+callback, which gets called on every MIDI event received and runs
+indenpendently from teh main loop.
+
+"""
+
+import time
+
+from rtmidi.midiutil import open_midiinput
+from rtmidi.midiconstants import (
+    CONTROL_CHANGE,
+    MODULATION,
+    CHANNEL_VOLUME,
+    EXPRESSION_CONTROLLER,
+)
+
+
+CONTROLLERS = (MODULATION, CHANNEL_VOLUME, EXPRESSION_CONTROLLER)
+
+
+class MidiInHandler:
+    def __init__(self, channel=1, controllers=None):
+        self.ch = channel
+        self.ccs = controllers or ()
+        self._cur_value = {}
+
+    def __call__(self, event, *args):
+        event, delta = event
+        status = event[0] & 0xF0
+        ch = event[0] & 0xF
+
+        if status == CONTROL_CHANGE and ch == self.ch - 1 and event[1] in self.ccs:
+            self._cur_value[event[1]] = event[2]
+
+    def get(self, cc, default=None):
+        return self._cur_value.get(cc, default)
+
+
+def main(args):
+    midiin, _ = open_midiinput(args[0] if args else None)
+
+    # record Modulation, Volume and Expression CC events
+    handler = MidiInHandler(channel=1, controllers=CONTROLLERS)
+    midiin.set_callback(handler)
+
+    try:
+        with midiin:
+            while True:
+                for cc in CONTROLLERS:
+                    print("CC #%i: %s" % (cc, handler.get(cc)))
+
+                print("--- ")
+                time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        del midiin
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
