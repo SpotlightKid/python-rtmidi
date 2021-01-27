389e232b66cd854b3486af17a0e9a8607f8da982
Christopher Arndt <chris@chrisarndt.de>
2021-01-27 18:50:43 +0100

    Add sysex sending example scripts
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

---
+++ examples/sysex/send_sysex.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import time
+
+import rtmidi
+from rtmidi.midiutil import open_midioutput
+
+
+def main(args=None):
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--port", help="MIDI output port")
+    ap.add_argument("sysex-byte", nargs="+",
+                    help="SysEx bytes as hexadecimal")
+    args = ap.parse_args()
+
+    midiout, name = open_midioutput(args.port)
+    print("Opened port '%s'." % name)
+
+    data = bytearray.fromhex("".join(getattr(args, "sysex-byte")))
+    assert data[0] == 0xF0 and data[-1] == 0xF7
+    print("Sending %d bytes" % len(data))
+    midiout.send_message(data)
+
+    time.sleep(0.5)
+    midiout.close_port()
+    del midiout
+
+
+if __name__ == "__main__":
+    sys.exit(main() or 0)
---
+++ examples/sysex/send_sysex_file.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import time
+
+import rtmidi
+from rtmidi.midiutil import open_midioutput
+
+
+def main(args=None):
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--port", help="MIDI output port")
+    ap.add_argument("-l", "--length", type=int,
+                    help="Maximum SysEx message length in bytes per file.")
+    ap.add_argument("sysex-file", nargs="+", help="SysEx input file(s)")
+    args = ap.parse_args()
+
+    midiout, name = open_midioutput(args.port)
+    print("Opened port '%s'." % name)
+
+    for filename in getattr(args, "sysex-file"):
+        with open(filename, 'rb') as syx:
+            data = syx.read()
+            assert data[0] == 0xF0 and data[-1] == 0xF7
+
+            if args.length:
+                data = bytearray(data[:args.length])
+
+            data[-1] = 0xF7
+            print("Sending %d bytes from '%s'..." % (len(data), filename))
+            midiout.send_message(data)
+
+    time.sleep(0.5)
+    midiout.close_port()
+    del midiout
+
+
+if __name__ == "__main__":
+    sys.exit(main() or 0)
