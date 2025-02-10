cad27eacfbd676ecf762ffaf85e31e6a1833fc3b
Christopher Arndt <chris@chrisarndt.de>
2025-02-10 18:09:37 +0100

    feat: Python 3.13 support and builds, drop Python 3.8 support
    - Set minimum required Python version 3.9 in `pyproject.toml`.
    - Added Python 3.13 to `cibuildwheel` build list in `pyproject.toml` and removed 3.8.
    - Added Python 3.13 to meta-data classifiers in `pyproject.toml` and removed 3.8.
    - Added Python 3.13 to `tox.ini` and removed 3.8.
    - Updated minimum supported Python version mentioned in readme etc.
    - Updated latest Python version mentioned in windows install docs.
    - ci: Use GitHub Action Linux arm runners, remove dedicated pipeline for aarch64.
    - ci: Let cibuildwheel install needed Python versions.
    
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- src/_rtmidi.pyx
+++ src/_rtmidi.pyx
@@ -19,7 +19,7 @@ and Windows (MultiMedia System) operating systems.
 provides a thin wrapper around the RtMidi C++ interface. The API is basically
 the same as the C++ one but with the naming scheme of classes, methods and
 parameters adapted to the Python PEP-8 conventions and requirements of the
-Python package naming structure. **python-rtmidi** supports Python 3 (3.8+).
+Python package naming structure. **python-rtmidi** supports Python 3 (3.9+).
 
 
 Usage example
